885dbc69aafbd7eb05ee9ea9be29091ccc10d0bc
root <root@ip-172-31-20-122.ec2.internal>
2022-06-15 14:57:51 +0000

    new exp

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from utils import load_data, load_data_from_npz
+from utils import *
 from MLP import mlp
 import torch
 import torchvision
@@ -9,13 +9,13 @@ import numpy as np
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 import pandas as pd
-from logentropy import MAELoss
+from logentropy import LogEntropyLoss, MAELoss
 
 
 
-epochs = 100
-lr = 0.1
-launchTimeStamp = "12_36_06_12"
+epochs = 150
+lr = 0.01
+launchTimeStamp = "18_07_06_15"
 label = "l1loss-10classes-card2"
 checkpoint = True
 
@@ -42,49 +42,71 @@ def train(train_loader, model, optimizer, train_criterion, use_cuda, epoch):
     return sum_loss
 
 
-def train(train_loader, model, optimizer, train_criterion, use_cuda, epoch):
+def train(train_loader, model, optimizer, train_criterion, watch_criterion, use_cuda, epoch):
     sum_loss = 0.0
+    true_loss = 0.0
+    l = len(train_loader)
     for data in train_loader:
-        img, label = data
+        img, (label, true_label) = data
         if use_cuda:
             img = Variable(img).cuda()
             label = Variable(label).cuda()
+            true_label = Variable(true_label).cuda()
         else:
             img = Variable(img)
             label = Variable(label)
+            true_label = Variable(true_label)
         out = model(img)
         loss = train_criterion(out, label)
         print_loss = loss.data.item()
-        sum_loss += print_loss
+        sum_loss += print_loss/l
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        with torch.no_grad():
+            loss = watch_criterion(out, true_label)
+            print_loss = loss.data.item()
+            true_loss += print_loss/l
+
     print('Epoch', epoch, 'Train Loss:', sum_loss)
-    return sum_loss
+    print('Epoch', epoch, 'True Loss:', true_loss)
+    return sum_loss, true_loss
 
 def train_pseodu(train_loader, model, optimizer, train_criterion, use_cuda, epoch):
+    total_correct = 0.0
+    total_num = 0.0
     sum_loss = 0.0
+    l = len(train_loader)
     for data in train_loader:
+        model.train()
         img, label = data
         bs = len(label)
-        label = label.unsqueeze(-1)
-        one_hot_label = torch.zeros(bs, 10).scatter_(1, label, 1)
+        #label = label.unsqueeze(-1)
+        #label = label.squeeze(-1)
+        #one_hot_label = torch.zeros(bs, 10).scatter_(1, label, 1)
         if use_cuda:
             img = Variable(img).cuda()
             label = Variable(label).cuda()
-            one_hot_label = Variable(one_hot_label).cuda()
+            #one_hot_label = Variable(one_hot_label).cuda()
         else:
             img = Variable(img)
             label = Variable(label)
-            one_hot_label = Variable(one_hot_label)
+            #one_hot_label = Variable(one_hot_label)
         out = model(img)
-        loss = train_criterion(out, one_hot_label)
+        pred = out.argmax(dim=1)
+        label_one = label.argmax(dim=1)
+        total_correct += torch.eq(pred,label_one).float().sum().item() #分别为是否相等，scalar tensor转换为float，求和，拿出值
+        total_num += label.size(0)
+        #loss = train_criterion(out, one_hot_label)
+        loss = train_criterion(out, label)
         print_loss = loss.data.item()
-        sum_loss += print_loss
+        sum_loss += print_loss/l
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+    acc = total_correct/total_num
     print('Epoch', epoch, 'Pseudo Train Loss:', sum_loss)
+    print('Epoch', epoch, 'Train Acc:', acc)
     return sum_loss
 
 
@@ -113,21 +135,29 @@ def watch(watch_loader, model, watch_criterion, use_cuda, epoch, l=None):
 def valid(valid_loader, model, use_cuda, epoch):
     total_correct = 0.0
     total_num = 0.0
+    total_loss = 0.0
     with torch.no_grad():
+        model.eval()
+        l = len(valid_loader)
         for data in valid_loader:
             val_inputs, label = data
             if use_cuda:
                 val_inputs = Variable(val_inputs).cuda()
                 label = Variable(label).cuda()
             val_outputs = model(val_inputs)
+            loss = torch.nn.CrossEntropyLoss().__call__(val_outputs, label)
+            loss = loss.data.item()
+            total_loss += loss/l
             pred = val_outputs.argmax(dim=1)
             total_correct += torch.eq(pred,label).float().sum().item() #分别为是否相等，scalar tensor转换为float，求和，拿出值
             total_num += label.size(0)
         acc = total_correct/total_num
         print('Epoch:', epoch, 'Val Acc:', acc)
-        return acc   
+        print('Epoch:', epoch, 'Val Loss:', total_loss)
+    return acc, total_loss
 
 def plot_curve(epochs, train_losses, pseudo_losses, true_losses, pseudo_true_losses, valid_accs, label):
+    plt.figure(figsize=(8, 12))
     epoch_num = epochs
     x1 = range(0, epoch_num)
     x2 = range(0, epoch_num)
@@ -166,6 +196,10 @@ def load_checkpoint(model, checkpoint_PATH, optimizer):
         optimizer.load_state_dict(model_CKPT['optimizer'])
     return model, optimizer
 
+def log_dict(tag, data):
+    df = pd.DataFrame(data)
+    df.to_csv('./logs/'+tag+'.csv')
+
 
 def main():
     use_cuda = torch.cuda.is_available()
@@ -181,49 +215,51 @@ def main():
     if use_cuda:
         model = model.cuda()
         model_psuedo = model_psuedo.cuda()
-    train_loader, watch_loader, psuedo_loader, valid_loader = load_data_from_npz()
-    train_criterion = MAELoss()
-    #ce_loss = LogEntropyLoss()
-    watch_criterion = MAELoss()
+    watch_loader, valid_loader = load_data_custom()
+    #train_loader, watch_loader, psuedo_loader, valid_loader = load_data_from_npz()
+    train_criterion = LogEntropyLoss()
+    watch_criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4)
-    optimizer_p = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4)
-    #model, optimizer = load_checkpoint(model, 'models/m-10_36_06_09-118.4261.pth.tar',optimizer)
-    #optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4)
-    # optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[256, 1000, 3000], gamma=0.1)
-    scheduler_p = torch.optim.lr_scheduler.MultiStepLR(optimizer_p, milestones=[256, 1000, 3000], gamma=0.1)
+    #model, optimizer = load_checkpoint(model, 'newmodels/m-22_15_06_13-123.0882.pth.tar',optimizer)
+   
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[60, 90, 120], gamma=0.1)
+    #scheduler_p = torch.optim.lr_scheduler.MultiStepLR(optimizer_p, milestones=[300, 1000, 3000], gamma=0.1)
     train_losses = []
     true_losses = []
-    pseudo_losses = []
-    pseudo_true_losses = []
+    # pseudo_losses = []
+    # pseudo_true_losses = []
+    valid_losses = []
     valid_accs = []
     for epoch in range(epochs):
-        train_loss = train(train_loader, model, optimizer, train_criterion, use_cuda, epoch)
+        train_loss, true_loss = train(watch_loader, model, optimizer, train_criterion, watch_criterion, use_cuda, epoch)
+        # train_losses.append(train_loss)
+        #pseudo_train_loss = train_pseodu(psuedo_loader, model_psuedo, optimizer_p, train_criterion, use_cuda, epoch)
+        #pseudo_losses.append(0)
+        #true_loss = watch(watch_loader, model, watch_criterion, use_cuda, epoch)
+        #pseudo_true_loss = watch(watch_loader, model_psuedo, watch_criterion, use_cuda, epoch, l='p')
         train_losses.append(train_loss)
-        pseudo_train_loss = train_pseodu(psuedo_loader, model_psuedo, optimizer_p, train_criterion, use_cuda, epoch)
-        pseudo_losses.append(pseudo_train_loss)
-        true_loss = watch(watch_loader, model, watch_criterion, use_cuda, epoch)
-        pseudo_true_loss = watch(watch_loader, model_psuedo, watch_criterion, use_cuda, epoch, l='p')
-        pseudo_true_losses.append(pseudo_true_loss)
         true_losses.append(true_loss)
-        val_acc = valid(valid_loader, model, use_cuda, epoch)
+        val_acc, val_loss = valid(valid_loader, model, use_cuda, epoch)
         valid_accs.append(val_acc)
+        valid_losses.append(val_loss)
+        scheduler.step()
         # if epoch % 200 == 1:
         #     log(str(epoch), train_losses, true_losses, valid_accs, entropy_losses, cons_losses)
         #scheduler.step()
         #scheduler_p.step()
-        if epoch % 1000 == 1 or epoch % 1000 == 999:
-            lossMIN = true_loss
-            torch.save({'epoch': epoch + 1, 'state_dict': model.state_dict(), 
-                            'optimizer': optimizer.state_dict()},
-                           './newmodels' + '/m-' + launchTimeStamp + '-' + str("%.4f" % lossMIN) + '.pth.tar')
-            lossMIN = pseudo_train_loss
-            torch.save({'epoch': epoch + 1, 'state_dict': model_psuedo.state_dict(), 
-                            'optimizer': optimizer_p.state_dict()},
-                           './newmodels' + '/p-' + launchTimeStamp + '-' + str("%.4f" % lossMIN) + '.pth.tar')
-            
+        # if epoch % 500 == 499:
+        #     lossMIN = true_loss
+        #     torch.save({'epoch': epoch + 1, 'state_dict': model.state_dict(), 
+        #                     'optimizer': optimizer.state_dict()},
+        #                    './newmodels' + '/m-' + launchTimeStamp + '-' + str("%.4f" % lossMIN) + '.pth.tar')
+            # lossMIN = pseudo_train_loss
+            # torch.save({'epoch': epoch + 1, 'state_dict': model_psuedo.state_dict(), 
+            #                 'optimizer': optimizer_p.state_dict()},
+            #                './newmodels' + '/p-' + launchTimeStamp + '-' + str("%.4f" % lossMIN) + '.pth.tar')
+    data = {'Train Loss':train_losses, 'True Loss':true_losses, 'Val Loss':valid_losses, 'Val Acc':valid_accs}
+    log_dict(launchTimeStamp, data)      
     #log(str(epoch), train_losses, true_losses, valid_accs, entropy_losses, cons_losses)
-    plot_curve(epochs, train_losses, pseudo_losses, true_losses, pseudo_true_losses, valid_accs, label)
+    #plot_curve(epochs, train_losses, pseudo_losses, true_losses, pseudo_true_losses, valid_accs, label)
 
 if __name__ == '__main__':
 
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import torch.nn as nn
 import torch
 import numpy as np
@@ -5,25 +6,60 @@ import math
 from sklearn.metrics import accuracy_score
 import torch.utils.data as Data
 import torch.utils.data 
+import torchvision
 from torchvision import datasets
+from zmq import TOS
 from subset import subset
 import torchvision.transforms as transforms
 import cifar10 as dataset
 from random import choice
+from collections import Counter
 
 
 batch_size = 100
 augment_K = 4
 
+sub = subset(10)
+
+# transform_train = transforms.Compose([
+#         dataset.RandomPadandCrop(32),
+#         dataset.RandomFlip(),
+#         dataset.ToTensor(),
+#         transforms.Normalize([0.395, 0.431, 0.516], [0.246, 0.223, 0.234])
+#     ])
+
+
+# transform_val = transforms.Compose([
+#     # dataset.RandomPadandCrop(32),
+#     # dataset.RandomFlip(),
+#     # dataset.Normalize(),
+#     dataset.ToTensor(),
+#     transforms.Normalize([0.395, 0.431, 0.516], [0.246, 0.223, 0.234])
+    
+# ])
+class ToSubset(object):
+    def __init__(self, mode=None):
+        self.mode = mode
+    
+
+    def __call__(self, y):
+        subset_y = sub.index_to_limited_subset(y)[1]
+        return torch.from_numpy(np.array(subset_y))
+
+transform_targets = ToSubset()
+
 transform_train = transforms.Compose([
-        dataset.RandomPadandCrop(32),
-        dataset.RandomFlip(),
-        dataset.ToTensor(),
-    ])
+    transforms.RandomCrop(32, padding=4),  #先四周填充0，在吧图像随机裁剪成32*32
+    transforms.RandomHorizontalFlip(),  #图像一半的概率翻转，一半的概率不翻转
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)), #R,G,B每层的归一化用到的均值和方差
+])
 
 transform_val = transforms.Compose([
-    dataset.ToTensor(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
+
 
 class MyDataset(Data.Dataset):
 
@@ -75,8 +111,8 @@ class MyDatasetWithAugmentation(Data.Dataset):
     def __len__(self):
         return len(self.data)
 
-def load_data(classes=[0,1,2,3,4,5,6,7,8,9]):
-    sub = subset(10)
+def load_data():
+    
 
     train_data = datasets.CIFAR10(root='.', train=True, download=True)
     test_data = datasets.CIFAR10(root='.', train=False, download=True)
@@ -85,86 +121,108 @@ def load_data(classes=[0,1,2,3,4,5,6,7,8,9]):
     x_test = test_data.data
     y_train = train_data.targets
     y_test = test_data.targets
+    
 
     X_train_true = np.zeros(shape=(50000, 3, 32, 32), dtype=float)
-    X_test_true = np.zeros(shape=(10000, 3, 32, 32), dtype=float)
+    validation_X = np.zeros(shape=(10000, 3, 32, 32), dtype=float)
 
-    # data preparation
+    validation_y = y_test
+    # X_test_true = np.zeros(shape=(10000, 3, 32, 32), dtype=float)
+
+    # # data preparation
     for index, sample in enumerate(x_train):
-        X_train_true[index] = sample.reshape(3,32,32).astype(float) / 255
+        X_train_true[index] = sample.reshape(3,32,32).astype(float)/255
+
     for index, sample in enumerate(x_test):
-        X_test_true[index] = sample.reshape(3,32,32).astype(float) / 255
+        validation_X[index] = sample.reshape(3,32,32).astype(float)/255
+
+    # for index, sample in enumerate(x_test):
+    #     X_test_true[index] = sample.reshape(3,32,32).astype(float) / 255
 
     # split
     from sklearn.model_selection import train_test_split
 
     train_X, train_y = X_train_true, y_train
 
-    #train_X, _, train_y, _ = train_test_split(X_train_true, y_train, test_size=0.05, random_state=42)
-    test_X, validation_X, test_y, validation_y = train_test_split(X_test_true, y_test, test_size=0.2, random_state=42)
+    #train_X, validation_X, train_y, validation_y = train_test_split(X_train_true, y_train, test_size=0.1, random_state=42)
 
-    print(len(validation_X))
+    #train_X, _, train_y, _ = train_test_split(X_train_true, y_train, test_size=0.05, random_state=42)
+    #test_X, validation_X, test_y, validation_y = train_test_split(X_test_true, y_test, test_size=0.2, random_state=42)
+
+    #print(len(validation_X))
+    #print(Counter(validation_y))
+    print(Counter(train_y))
 
     test_labels = []
     train_labels = []
-    X_validation = []
-    validation_labels = []
+    #X_validation = []
+    #validation_labels = []
     true_labels = []
     psuedo_labels =[]
-    X_train = []
+    #X_train = []
     X_test = []
     monitor = [0,0,0,0]
 
 
     for index, y in enumerate(train_y):
-        if y in classes:
-            #size_of_subset, subset_y, obfuscated_y = sub.index_to_stack_obfuscated(y)
-            sub_label, multi_hot_subset = sub.index_to_limited_subset(y, max_class=10)
-            X_train.append(train_X[index])
-            train_labels.append(multi_hot_subset)
-            true_labels.append(y)
-            psuedo_labels.append(choice(sub_label))
-            # if y == 0:
-            #     for i in sub_label:
-            #         monitor[i] += 1
+        #size_of_subset, subset_y, obfuscated_y = sub.index_to_stack_obfuscated(y)
+        sub_label, multi_hot_subset = sub.index_to_limited_subset(y, max_class=10)
+        #X_train.append(train_X[index])
+        train_labels.append(multi_hot_subset)
+        #true_labels.append(y)
+        psuedo_labels.append(choice(sub_label))
+        # if y == 0:
+        #     for i in sub_label:
+        #         monitor[i] += 1
     
+    true_labels = train_y
     #print(monitor)
 
 
-    for index, y in enumerate(test_y):
-        if y in classes:
-            X_test.append(test_X[index])
-            test_labels.append(y)
+    # for index, y in enumerate(test_y):
+    #     if y in classes:
+    #         X_test.append(test_X[index])
+    #         test_labels.append(y)
 
-    for index,y in enumerate(validation_y):
-        if y in classes:
-            X_validation.append(validation_X[index])
-            validation_labels.append(y)
+    # for index,y in enumerate(validation_y):
+    #     if y in classes:
+    #         X_validation.append(validation_X[index])
+    #         validation_labels.append(y)
 
     print(torch.utils.data.dataloader.__file__)
-
-
   
-    input = np.array(X_train).astype(np.float32)
+    input = np.array(train_X).astype(np.float32)
+    watch_input = deepcopy(input)
     label = np.array(train_labels).astype(np.int64)
-    true_labels = np.array(true_labels).astype(np.long)
+    true_labels = np.array(train_y).astype(np.long)
     psuedo_labels = np.array(psuedo_labels).astype(np.long)
     np.savez('./data/train.npz', train_x=input, subset_y=label, true_y=true_labels, pseudo_y=psuedo_labels)
-    torch_dataset = MyDataset(input, label, transform_train)
+    train_dataset = MyDataset(input, label, transform_train)
     #torch_dataset_withaug = MyDatasetWithAugmentation(input, label, transform_train, augment_K)
-    watch_dataset = MyDataset(input, true_labels, transform_val)
-    train_loader = Data.DataLoader(torch_dataset, batch_size=batch_size, shuffle=True)
+    watch_dataset = MyDataset(watch_input, true_labels, transform_train)
+    train_loader = Data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     #train_loader_augmentation = Data.DataLoader(torch_dataset_withaug, batch_size=batch_size, shuffle=True)
-    watch_loader = Data.DataLoader(watch_dataset, batch_size=batch_size, shuffle=False)
+    watch_loader = Data.DataLoader(watch_dataset, batch_size=batch_size, shuffle=True)
 
 
-    input = np.array(X_validation).astype(np.float32)
-    label = np.array(validation_labels).astype(np.long)
-    np.savez('./data/valid.npz', valid_x=input, valid_y=label)
-    torch_dataset = MyDataset(input, label, transform_val)
-    valid_loader = Data.DataLoader(torch_dataset, batch_size=batch_size, shuffle=True)
+    val_input = np.array(validation_X).astype(np.float32)
+    val_label = np.array(validation_y).astype(np.long)
+    np.savez('./data/valid.npz', valid_x=val_input, valid_y=val_label)
+    valid_dataset = MyDataset(val_input, val_label, transform_val)
+    valid_loader = Data.DataLoader(valid_dataset, batch_size=batch_size, shuffle=True)
 
     return train_loader, watch_loader, valid_loader
+
+def load_data_custom():
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train, target_transform=transform_targets) #训练数据集
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)   #生成一个个batch进行批训练，组成batch的时候顺序打乱取
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_val)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False)
+    return trainloader, testloader
+
+
+
+
 
 def load_data_from_npz(mode='subset'):
     data_train = np.load('./data/train.npz')
